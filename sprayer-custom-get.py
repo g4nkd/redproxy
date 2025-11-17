abe4db8c340cb284cae90514a5120bb94f5bab0d
raceconditionss <jadxgui@gmail.com>
2025-11-16 23:24:38 -0300

    Update sprayer-custom-get.py

--- sprayer-custom-get.py
+++ sprayer-custom-get.py
@@ -17,7 +17,7 @@ catcher_uses_TLS_str = os.getenv("CATCHERTLS")
 catcher_uses_TLS = catcher_uses_TLS_str.lower() == "true"
 
 def send_login_request():
-    url = "https://cloudflare.manfredi.io/test/"
+    url = "http://18.234.239.10/test/"
     post_headers = {
         "Accept": "application/json",
         "Content-Type": "application/x-www-form-urlencoded",
